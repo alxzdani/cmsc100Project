a694fd59dcd0254af34eb4cadad60a0edca29796
Alexis Danielle Aguinaldo <159098132+alxzdani@users.noreply.github.com>
2024-05-10 10:22:34 +0800

    feat: added admin login auth

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './pages/landing_page';
 import Signup from './pages/signup';
 import Login from './pages/login';
 import ShopPage from './pages/shop';
+import Dashboard from './pages/adminPages/admin-dashboard';
 
 //log out implemented but not in proper page
 
@@ -15,9 +16,13 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
-        {/* asking if user logged in and if they are they can access shop page */}
+    
+        {/* asking if user logged in and if they are they can access shop page and admin dashboard */}
         {isUserLogIn && <Route path="/shop" element={<ShopPage />} />}
+
+        {/* admin dashboard should be protected */}
+        {isUserLogIn && <Route path="/admin-dashboard" element={<Dashboard />} />}
+
       </Routes>
     </div>
   );
