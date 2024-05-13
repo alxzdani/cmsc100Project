cb1ebda5c49b27387d21dd2e6790ee5edb2bf1f6
aerondain <aerondain@up.edu.ph>
2024-05-13 19:25:49 +0800

    ui: updated landing page, login, navbar buttons
    - added farm-to-table background image as asset
    - set as bg image for landing page and login
    - updated signup and login buttons on navbar
    - farm-to-table logo clickable, returns to landing page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,9 +7,10 @@ module.exports = {
         "green": "#10AB4E",
         "lime": "#00DD00",
         "notblack": "#1A1A1A",
+        "notwhite": "#F8F8F8",
         "grey": "#737882",
         "lightgrey": "#7A7E7D"
-    },
+      },
     },
   },
   plugins: [],
