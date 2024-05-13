166ff0e3652d5e42dc4384072d7357231fc31a09
hbsamson <hbsamson@up.edu.ph>
2024-05-14 00:06:53 +0800

    refactor: ready for merging with fe-signup-login

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -4,22 +4,29 @@ import BG_IMG from "../assets/farm-to-table-bg.jpg";
 
 export default function LandingPage() {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${BG_IMG})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        minHeight: '100vh',
-        width: '100vw',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-      id="top"
-    >
+    <div className="bg-backgroundColor h-full w-screen" id="top">
       <Navbar />
+      <div className="flex flex-row items-center px-2 my-10 py-10" id="main">
+              <div className="flex flex-col ml-10 mr-10">
+              <div style={{
+                  backgroundImage: `url(${BG_IMG})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                  minHeight: '100vh',
+                  width: '100vw',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                id="top"
+              >
+            </div>            
+          </div>
+          <Footer />
+      </div>
+
     </div>
   );
 }
