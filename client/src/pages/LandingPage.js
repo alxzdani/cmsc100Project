4e3dd673a2d67239471d42e32cfe1dfe2f63ad9e
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-05-14 01:05:17 +0800

    refactor: convert css image styling to tailwind

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,32 +1,15 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import BG_IMG from "../assets/farm-to-table-bg.jpg";
+import BG from "../assets/bg.jpg";
 
 export default function LandingPage() {
   return (
     <div className="bg-backgroundColor h-full w-screen" id="top">
       <Navbar />
-      <div className="flex flex-row items-center px-2 my-10 py-10" id="main">
-              <div className="flex flex-col ml-10 mr-10">
-              <div style={{
-                  backgroundImage: `url(${BG_IMG})`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                  backgroundRepeat: 'no-repeat',
-                  minHeight: '100vh',
-                  width: '100vw',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-                id="top"
-              >
-            </div>            
-          </div>
-          <Footer />
+      <div className="flex flex-col items-center my-10 pb-10" id="main">
+          <img src={BG} className="w-screen h-screen overflow-auto object-cover object-center" alt="Background Image" />
       </div>
-
+      <Footer />
     </div>
   );
 }
