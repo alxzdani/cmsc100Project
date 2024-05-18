862a324d3a787c567b22e9d0524b5003003ea387
Alexis Danielle Aguinaldo <159098132+alxzdani@users.noreply.github.com>
2024-05-19 02:16:07 +0800

    feat: sorting features

--- client/src/pages/adminPages/ProductListing.js
+++ client/src/pages/adminPages/ProductListing.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function ProductListing() {
-    const isUserLogIn = localStorage.getItem('token')
-    const navigate = useNavigate()
-    
+    const isUserLogIn = localStorage.getItem('token');
+    const navigate = useNavigate();
+
     const handleLogout = () => {
-        localStorage.removeItem('token')
-        navigate('/login')
-    }
-    
+        localStorage.removeItem('token');
+        navigate('/login');
+    };
+
     const [products, setProducts] = useState([]);
-    
+    const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
+
     // Connect to API
     const fetchProducts = () => {
         axios.get('http://localhost:3001/shop')
@@ -23,26 +24,56 @@ function ProductListing() {
             .catch((error) => {
                 console.error('Error fetching products:', error);
             });
-    }
+    };
 
     useEffect(() => {
         fetchProducts();
-    }, [])
+    }, []);
 
+    const sortProducts = (key) => { // key determines the product to be sorted
+        let direction = 'ascending';
+        
+        // if it is true that the sorting condition is currently ascending, toggle it to become descending
+        if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending'; 
+        }
+
+        const sortedProducts = [...products].sort((a, b) => {
+
+            //if it is currently ascending, make it descending
+            if (a[key] < b[key]) return direction === 'ascending' ? -1 : 1;
+            
+            //if it is currently descending, make it ascending
+            if (a[key] > b[key]) return direction === 'ascending' ? 1 : -1;
+            return 0;
+        });
+
+        setProducts(sortedProducts); // render sorted products
+
+        setSortConfig({ key, direction });  // accepting the product and the direction
+    };
 
     return (
         <div>
-          {/* a header, when click it will direct to the shopping page */}
-            <Link to='/shop'> Shopping Page </Link>  
+            {/* a header, when clicked, it will direct to the shopping page */}
+            <Link to='/shop'> Shopping Page </Link>
             <div>
                 {isUserLogIn ? (
                     // if the user is signed in we want to render out signout button
                     // and the list of products
-                    <>  
-                    
+                    <>
                         <li><button onClick={handleLogout}> Log Out</button></li>
-
                         <div>
+                            {/* Sorting buttons or dropdown menus */}
+                            <button onClick={() => sortProducts('productName')}>Sort by Name</button>
+                            <br/>
+                            <button onClick={() => sortProducts('productType')}>Sort by Type</button>
+                            <br/>
+                            <button onClick={() => sortProducts('productPrice')}>Sort by Price</button>
+                            <br/>
+                            <button onClick={() => sortProducts('productQuantity')}>Sort by Quantity</button>
+                            <br/>
+
                             {products.map((product) => {
                                 return (
                                     <div key={product._id}>
@@ -55,20 +86,18 @@ function ProductListing() {
                                     </div>
                                 )
                             })}
-
                         </div>
-                        
                     </>
                 ) : (
-                        // if they are not logged in
-                        <>
-                            <Link to='/login'><li>Log in</li></Link>
-                            <Link to='/signup'><li>Sign up</li></Link>
-                        </>
-                    )}
+                    // if they are not logged in
+                    <>
+                        <Link to='/login'><li>Log in</li></Link>
+                        <Link to='/signup'><li>Sign up</li></Link>
+                    </>
+                )}
             </div>
         </div>
-    )
+    );
 }
 
-export default ProductListing
+export default ProductListing;
