153a8bdc966f864bfadc16b65a20aca325a5f3ff
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-05-19 17:49:00 +0800

    style: added lighter grey color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         "notblack": "#1A1A1A",
         "notwhite": "#F8F8F8",
         "grey": "#737882",
-        "lightgrey": "#7A7E7D"
+        "greyer": "#7A7E7D",
+        "lightgrey": "#C8C8C8"
     },
     },
   },
