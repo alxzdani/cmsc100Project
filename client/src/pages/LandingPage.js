b87b612e57589747da0964cdb6512f6965c8e782
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-05-19 18:36:51 +0800

    fix: removed extra horizontal scroll

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -4,10 +4,10 @@ import BG from "../assets/bg.jpg";
 
 export default function LandingPage() {
   return (
-    <div className="bg-backgroundColor h-full w-screen" id="top">
+    <div className="" id="top">
       <Navbar />
-      <div className="flex flex-col items-center my-10 pb-10" id="main">
-          <img src={BG} className="w-screen h-screen overflow-auto object-cover object-center" alt="Background Image" />
+      <div className="flex flex-col items-center mt-22 my-10 pb-10" id="main">
+          <img src={BG} className="h-screen overflow-auto object-cover object-top" alt="Background Image" />
       </div>
       <Footer />
     </div>
