8aa3db5107378c568e4d23fa6cae3ef809cde4ca
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-05-21 16:05:44 +0800

    feat: add admin dashboard

--- client/src/components/AdminNavbar.js
+++ client/src/components/AdminNavbar.js
@@ -1,20 +1,36 @@
 import { LayoutDashboard, BookUser, ShoppingCart, ShoppingBasket, PieChart, LogOut } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+import LOGO from "../assets/logo.png";
 
 export default function AdminNavbar () {
+    const isAdminLogIn = localStorage.getItem('userType') === 'admin';
+
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem('token')
+        navigate('/login')
+    }
+
     return (
         <div>
-            <div className="">
-            <div className="flex flex-col w-1/4 rounded-r-xl h-screen bg-notblack p-10">
-                <h1 className="text-white">Hello, Admin!</h1>
-                <div className="space-y-5">
-                    <button className="border-2 bg-white bg-opacity-10 flex flex-row w-56 p-3 mx-auto rounded-lg text-white"><LayoutDashboard className="mr-2"/>Dashboard</button>
-                    <button className="border-2 bg-white bg-opacity-10 flex flex-row w-56 p-3 mx-auto rounded-lg text-white"><BookUser className="mr-2"/>User Management</button>
-                    <button className="border-2 bg-white bg-opacity-10 flex flex-row w-56 p-3 mx-auto rounded-lg text-white"><ShoppingCart className="mr-2"/>Product Listing</button>
-                    <button className="border-2 bg-white bg-opacity-10 flex flex-row w-56 p-3 mx-auto rounded-lg text-white"><ShoppingBasket className="mr-2"/>Order Fulfilment</button>
-                    <button className="border-2 bg-white bg-opacity-10 flex flex-row w-56 p-3 mx-auto rounded-lg text-white"><PieChart className="mr-2"/>Sales Report</button>
+            <div className="z-50">
+            <div className="flex flex-col rounded-r-xl h-screen bg-notblack p-10">
+                <div className="">
+                    <img src={LOGO} className="size-48 rounded-lg p-5 mx-auto bg-white" alt="Logo" />
+                    <h1 className="text-2xl text-lime my-5">Hello, Admin!</h1>
                 </div>
+               
+                <div className="space-y-5">
+                    <button onClick={() => navigate("/admin-dashboard")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><LayoutDashboard className="mr-2"/>Dashboard</button>
+                    <button onClick={() => navigate("/product-listing")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><BookUser className="mr-2"/>User Management</button>
+                    <button onClick={() => navigate("/user-management")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><ShoppingCart className="mr-2"/>Product Listing</button>
+                    <button onClick={() => navigate("/order-fulfillment")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><ShoppingBasket className="mr-2"/>Order Fulfilment</button>
+                    <button onClick={() => navigate("/sales-report")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><PieChart className="mr-2"/>Sales Report</button>
+                </div>
+                
                 <div className="m-auto"></div>
-                <button className="flex flex-row text-white mx-auto"><LogOut className="mr-2"/>Logout</button>
+                <button className="flex flex-row text-white ml-5" onClick={handleLogout}><LogOut className="mr-2"/>Logout</button>
             </div>
             <div className=""></div>
             </div>
--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import AdminNavbar from "../components/AdminNavbar"
+import ORDER from "../assets/order.png";
+import PRODUCT from "../assets/product.png";
+import SALES from "../assets/sales.png";
+import USER from "../assets/user.png";
 
 function Dashboard() {
     const isAdminLogIn = localStorage.getItem('userType') === 'admin';
@@ -17,17 +21,35 @@ function Dashboard() {
   return (
     <div>
         <div>
-           <AdminNavbar />
-
           </div>
             {isAdminLogIn  ? ( // if the user is signed in we want to render out signout button
                 <>
-                <Link to='/product-listing'> <li>Product Listing</li> </Link>
-                <Link to='/user-management'> <li>User Management</li> </Link>
-                <Link to='/sales-report'> <li>Sales Report</li> </Link>
-                <Link to='/order-fulfillment'> <li>Order Fulfillment</li> </Link>
-                <br/>
-                <li><button onClick={handleLogout}> Log Out</button></li>
+                <div className="flex flex-row">
+                    <div className="w-1/4">
+                    <AdminNavbar />
+                    </div>
+                    
+                    <div className="w-3/4">
+                        <div className="w-11/12 mx-auto mt-10">
+                            <h1 className="text-3xl text-green border-b-2 font-semibold border-green pb-5 text-left">Dashboard</h1>
+                        </div>
+                        <div className="grid gap-12 grid-cols-2 grid-cols-2 p-12 mx-auto">
+                            <button className="h-60 rounded-lg">
+                                <img src={USER} className=""/>
+                                <span className="ml-56 z-50 bg-[#858585] text-white p-2 w-48 rounded-lg mix-blend-multiply">User Management</span></button>
+                            <button className="h-60 rounded-lg">
+                                <img src={PRODUCT} className=""/>
+                                <span className="ml-40  bg-[#858585] text-white p-2 w-48 rounded-lg mix-blend-multiply">Product Listing</span></button>
+                            <button className="h-60 rounded-lg">
+                                <img src={ORDER} className=""/>
+                                <span className="ml-40  bg-[#858585] text-white p-2 w-48 rounded-lg mix-blend-multiply">Order Management</span></button>
+                            <button className="h-60 rounded-lg">
+                                <img src={SALES} className=""/>
+                                <span className="ml-40  bg-[#858585] text-white p-2 w-48 rounded-lg mix-blend-multiply">Sales Report</span></button>
+                        
+                        </div>
+                    </div>
+                </div>
                 </>
             ): (
                 //if they are not logged in
