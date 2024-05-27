9d17bc385e08fbe6e37edfc6bbdec1a3f68d3f08
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-05-27 20:02:44 +0800

    feat: productQuantity prompts for 0

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -38,38 +38,47 @@ export default function ProductCard({ product, onAddToCart }) {
                     <p className="text-grey text-sm">
                         {product.productType === 1 ? 'Crop' : 'Poultry'}
                     </p>
+                    <p className="text-notblack text-sm">
+                        {product.productQuantity === 0 ? 'Out of stock' : 'Stocks Left: ' + `${product.productQuantity}`}
+                    </p>
                 </div>
                 <span className="m-auto"></span>
-                <button type="button" className="px-2 text-lightgrey rounded-md hover:bg-gray-50" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} id="menu-button" aria-expanded="true" aria-haspopup="true"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-info"><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg></button>
-                {/* <div class="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-                    <div class="py-1" role="none">
-                        <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
-                        <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
-                        <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
+                <div className="flex flex-row">
+                    <button type="button" className="px-2 text-lightgrey rounded-md hover:bg-gray-50" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} id="menu-button" aria-expanded="true" aria-haspopup="true"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-info"><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg></button>
+                    {isHovering && (
+                    <div className="absolute mt-2 origin-top-right bg-white shadow-lg ring-1 ring-black ring-opacity-5 p-4 rounded-md w-auto max-w-full">
+                        <p className="whitespace-pre-wrap">{product.productDesc}</p>
                     </div>
-                </div> */}
+                    )}
+                </div>
             </div>
 
             <div className="flex flex-row mx-auto mb-5">
                 <div className="m-auto"></div>
                 <p className="text-right font-bold text-green">Php {product.productPrice}</p>
             </div>
-            <button
-                className="bg-green text-white rounded-lg px-16 py-2 text-lg self-center"
-                onClick={handleAddToCart}
-            >Add to Cart</button>
-            <p className="">{product.productQuantity} Stocks Left</p>
+            {product.productQuantity === 0 ? [
+                <button
+                className="bg-lightgrey text-white rounded-lg px-16 py-2 text-lg self-center"
+                disabled
+                >Add to Cart</button>
+            ] : [
+                <button
+                    className="bg-green text-white rounded-lg px-16 py-2 text-lg self-center"
+                    onClick={handleAddToCart}
+                >Add to Cart</button>
+            ]
+            }
+            
+            
             {isInCart && (
                 <div className="flex flex-row space-x-5 my-5 items-center justify-center">
                     {counter > 1 && <button className="bg-red-500 text-white rounded-full px-2 text-lg" onClick={decreaseCount}>-</button>}
                     <p className="">{counter}</p>
                     <button className="bg-green text-white rounded-full px-2 text-lg" onClick={increaseCount}>+</button>
                 </div>
-            )}            {isHovering && (
-                <div className="absolute mt-2 origin-top-right bg-white shadow-lg ring-1 ring-black ring-opacity-5 p-4 rounded-md w-auto max-w-full">
-                    <p className="whitespace-pre-wrap">{product.productDesc}</p>
-                </div>
-            )}
+            )}           
+            
 
         </div>
     )
