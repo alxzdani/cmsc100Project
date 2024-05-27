1fdc74b08b957a43973f86553e07a918e72f32c4
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-05-27 18:29:25 +0800

    refactor: fix wrong routing

--- client/src/components/AdminNavbar.js
+++ client/src/components/AdminNavbar.js
@@ -23,8 +23,8 @@ export default function AdminNavbar () {
                
                 <div className="space-y-5">
                     <button onClick={() => navigate("/admin-dashboard")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><LayoutDashboard className="mr-2"/>Dashboard</button>
-                    <button onClick={() => navigate("/product-listing")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><BookUser className="mr-2"/>User Management</button>
-                    <button onClick={() => navigate("/user-management")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><ShoppingCart className="mr-2"/>Product Listing</button>
+                    <button onClick={() => navigate("/user-management")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><BookUser className="mr-2"/>User Management</button>
+                    <button onClick={() => navigate("/product-listing")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><ShoppingCart className="mr-2"/>Product Listing</button>
                     <button onClick={() => navigate("/order-fulfillment")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><ShoppingBasket className="mr-2"/>Order Fulfilment</button>
                     <button onClick={() => navigate("/sales-report")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><PieChart className="mr-2"/>Sales Report</button>
                 </div>
