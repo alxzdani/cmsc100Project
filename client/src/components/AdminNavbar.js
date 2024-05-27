af9ebfd8d2e4f83757a32cf95606baa0095f2e1f
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-05-28 01:49:24 +0800

    feat: hideaway admin navbar

--- client/src/components/AdminNavbar.js
+++ client/src/components/AdminNavbar.js
@@ -1,9 +1,11 @@
-import { LayoutDashboard, BookUser, ShoppingCart, ShoppingBasket, PieChart, LogOut } from 'lucide-react'
+import React, { useState } from 'react'
+import { AlignJustify, LayoutDashboard, BookUser, ShoppingCart, ShoppingBasket, PieChart, LogOut } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 import LOGO from "../assets/logo.png";
 
 export default function AdminNavbar () {
     const isAdminLogIn = localStorage.getItem('userType') === 'admin';
+    const [navbarOpen, setNavbarOpen] = useState(false);
 
     const navigate = useNavigate()
 
@@ -14,25 +16,28 @@ export default function AdminNavbar () {
 
     return (
         <div>
+            <button className="fixed top-5 left-5 p-2 rounded-full text-white" onClick={() => setNavbarOpen(!navbarOpen)}>
+                {navbarOpen ? <AlignJustify /> : <AlignJustify />}
+            </button>
             <div className="z-50">
-            <div className="flex flex-col rounded-r-xl h-screen bg-notblack p-10">
-                <div className="">
-                    <img src={LOGO} className="size-48 rounded-lg p-5 mx-auto bg-white" alt="Logo" />
-                    <h1 className="text-2xl text-lime my-5">Hello, Admin!</h1>
-                </div>
-               
-                <div className="space-y-5">
-                    <button onClick={() => navigate("/admin-dashboard")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><LayoutDashboard className="mr-2"/>Dashboard</button>
-                    <button onClick={() => navigate("/user-management")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><BookUser className="mr-2"/>User Management</button>
-                    <button onClick={() => navigate("/product-listing")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><ShoppingCart className="mr-2"/>Product Listing</button>
-                    <button onClick={() => navigate("/order-fulfillment")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><ShoppingBasket className="mr-2"/>Order Fulfilment</button>
-                    <button onClick={() => navigate("/sales-report")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><PieChart className="mr-2"/>Sales Report</button>
-                </div>
-                
-                <div className="m-auto"></div>
-                <button className="flex flex-row text-white ml-5" onClick={handleLogout}><LogOut className="mr-2"/>Logout</button>
-            </div>
-            <div className=""></div>
+                <div className={`flex flex-col rounded-r-xl h-screen bg-notblack p-10 ${navbarOpen ? 'w-full' : 'w-20'}`}>
+                    <div className="">
+                        <img src={LOGO} className={`size-48 rounded-lg p-5 mx-auto bg-white ${navbarOpen ? 'block' : 'hidden'}`} alt="Logo" />
+                        <h1 className={`text-2xl text-lime my-5 ${navbarOpen ? 'block' : 'hidden'}`}>Hello, Admin!</h1>
+                    </div>
+                   
+                    <div className={`space-y-5 ${navbarOpen ? 'block' : 'hidden'}`}>
+                        <button onClick={() => navigate("/admin-dashboard")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><LayoutDashboard className="mr-2"/>Dashboard</button>
+                        <button onClick={() => navigate("/user-management")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><BookUser className="mr-2"/>User Management</button>
+                        <button onClick={() => navigate("/product-listing")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><ShoppingCart className="mr-2"/>Product Listing</button>
+                        <button onClick={() => navigate("/order-fulfillment")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><ShoppingBasket className="mr-2"/>Order Fulfilment</button>
+                        <button onClick={() => navigate("/sales-report")} className="bg-white bg-opacity-10 flex flex-row w-11/12 p-3 mx-auto rounded-lg text-white"><PieChart className="mr-2"/>Sales Report</button>
+                    </div>
+                    
+                    <div className={`m-auto ${navbarOpen ? 'block' : 'hidden'}`}></div>
+                        <button className={`flex flex-row text-white ml-5 ${navbarOpen ? 'block' : 'hidden'}`} onClick={handleLogout}><LogOut className="mr-2"/>Logout</button>
+                    </div>
+                <div className=""></div>
             </div>
         </div>
 
