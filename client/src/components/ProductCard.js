86f27a141dc52c600f912a251c51a98dad1cefbf
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-05-28 00:05:54 +0800

    refactor: in cart functionality

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,25 +1,22 @@
 import React, { useState } from 'react';
 export default function ProductCard({ product, onAddToCart }) {
-    const [isInCart, setIsInCart] = useState(true);
-    const [counter, setCounter] = useState(1)
     const [isHovering, setIsHovering] = useState(false);
     const [cartQuantity, setCartQuantity] = useState(0);
 
     const handleAddToCart = () => {
-        for (let i = 0; i < counter; i++) {
+        if (cartQuantity === 0) {
             onAddToCart(product);
         }
-        setCounter(1)
-        console.log(counter)
+        setCartQuantity(cartQuantity + 1);
     };
 
-    const decreaseCount = () => {
-        setCounter(counter - 1)
-    }
-
-    const increaseCount = () => {
-        setCounter(counter + 1)
-    }
+    const handleRemoveFromCart = () => {
+        if (cartQuantity > 1) {
+            setCartQuantity(cartQuantity - 1);
+        } else if (cartQuantity === 1) {
+            setCartQuantity(cartQuantity - 1);
+        }
+    };
 
     const handleMouseEnter = () => {
         setIsHovering(true);
@@ -43,7 +40,7 @@ export default function ProductCard({ product, onAddToCart }) {
                     </p>
                 </div>
                 <span className="m-auto"></span>
-                <div className="flex flex-row">
+                <div className="flex flex-row space-x-10">
                     <button type="button" className="px-2 text-lightgrey rounded-md hover:bg-gray-50" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} id="menu-button" aria-expanded="true" aria-haspopup="true"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-info"><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg></button>
                     {isHovering && (
                     <div className="absolute mt-2 origin-top-right bg-white shadow-lg ring-1 ring-black ring-opacity-5 p-4 rounded-md w-auto max-w-full">
@@ -64,22 +61,19 @@ export default function ProductCard({ product, onAddToCart }) {
                 >Add to Cart</button>
             ] : [
                 <button
-                    className="bg-green text-white rounded-lg px-16 py-2 text-lg self-center"
-                    onClick={handleAddToCart}
+                className="bg-green text-white rounded-lg px-16 py-2 text-lg self-center"
+                onClick={handleAddToCart}
                 >Add to Cart</button>
-            ]
-            }
+            ]}
             
-            
-            {isInCart && (
+            {cartQuantity > 0 && (
                 <div className="flex flex-row space-x-5 my-5 items-center justify-center">
-                    {counter > 1 && <button className="bg-red-500 text-white rounded-full px-2 text-lg" onClick={decreaseCount}>-</button>}
-                    <p className="">{counter}</p>
-                    <button className="bg-green text-white rounded-full px-2 text-lg" onClick={increaseCount}>+</button>
+                    <button className="bg-red-500 text-white rounded-full px-2 text-lg" onClick={handleRemoveFromCart}>-</button>
+                    <p className="">{cartQuantity} in cart</p>
+                    <button className="bg-green text-white rounded-full px-2 text-lg" onClick={handleAddToCart}>+</button>
                 </div>
-            )}           
+            )}       
             
-
         </div>
     )
 }
