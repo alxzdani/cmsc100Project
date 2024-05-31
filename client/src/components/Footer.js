00043cacf8a5dbebac07c3e5c19cd5001a124008
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-05-31 19:34:56 +0800

    fix: ui bugs

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     const navigate = useNavigate();
     
     return(
-        <div className="bg-white h-fit px-6 flex flex-row place-content-center">
+        <div className="bg-white h-fit px-6 flex flex-row place-content-center pb-10">
             <div className="m-auto flex flex-col w-screen">
 
                 <div className="bg-notblack w-11/12 flex flex-row place-items-center place-self-center rounded-md p-6">
@@ -20,9 +20,9 @@ export default function Footer() {
                 </div>
 
             <div className="flex flex-row w-11/12 place-self-center mt-20">
-                <div className="flex flex-col place-self-center text-left text-lightgrey">
+                <div className="flex flex-col place-self-center text-left text-gray-500">
                     <img src={LOGO} className="size-12 mb-5" alt="Logo" />
-                    <h1 className="text-notblack font-bold mb-5">Farm ni Ville</h1>
+                    <h1 className="text-notblack font-bold mb-5 text-2xl">Farm ni Ville</h1>
                     <p className="">Project under the Department of Agriculture</p>
                 </div>
                 
@@ -31,34 +31,23 @@ export default function Footer() {
                 </div>
 
                 <div className="flex flex-row justify-end my-10 space-x-40 mr-10 text-left">
-                    <div className="flex flex-col items-start">
-                        <h3 className="text-notgreen text-lg mb-2 flex flex-row">Policies</h3>
-                        <Link to="/link">
-                        <p className="text-lightgrey text-sm mb-1">Terms of Service</p>
-                        </Link>
-                        <Link to="/link">
-                        <p className="text-lightgrey text-sm mb-1">Merchant Guidelines</p>
-                        </Link>
-                        <Link to="/link">
-                        <p className="text-lightgrey text-sm mb-1">Privacy Policy</p>
-                        </Link>
-                    </div>
+                   
 
                     <div className="flex flex-col items-start">
-                        <h3 className="text-notgreen text-lg mb-2 flex flex-row">Quick Links</h3>
+                        <h3 className="text-notgreen text-lg mb-2 flex flex-row font-semibold">Quick Links</h3>
                         <Link to="/link">
                         <p className="text-lightgrey text-sm mb-1">Customer Support</p>
                         </Link>
                         <Link to="/link">
                         <p className="text-lightgrey text-sm mb-1">Shipping and Returns</p>
                         </Link>
-                        <Link to="/link">
+                        <Link to="/">
                         <p className="text-lightgrey text-sm mb-1">FAQ</p>
                         </Link>
                     </div>
 
                     <div className="flex flex-col items-start">
-                        <h3 className="text-notgreen text-lg mb-2 flex flex-row">Connect with Us</h3>
+                        <h3 className="text-notgreen text-lg mb-2 flex flex-row font-semibold">Connect with Us</h3>
                         <div className="flex flex-row text-lightgrey space-x-3">
                             <svg class="lucide lucide-facebook" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
                             <svg class="lucide lucide-instagram" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
