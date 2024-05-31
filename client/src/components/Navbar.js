040828dc387b13f3bf893c81fd2faea6e40d4b32
Hannah Patricia Samson <hbsamson@up.edu.ph>
2024-05-31 19:53:03 +0800

    style: restyle shop, cart, orders button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,7 +2,7 @@ import LOGO from "../assets/logo.png";
 import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import { useSnackbar } from '../components/SnackbarContext';
-import { CircleX, CircleCheckBig } from 'lucide-react'
+import { CircleX, CircleCheckBig, Store, ShoppingCart, ShoppingBag } from 'lucide-react'
 
 export default function Navbar() {
     const navigate = useNavigate();
@@ -28,6 +28,20 @@ export default function Navbar() {
                     <img src={LOGO} className="size-14 ml-6 mr-3 place-self-center" alt="Logo" />
                     <h1 className="text-notblack tracking-wide text-3xl font-bold mr-5 place-self-center">Farm ni Ville</h1>
                 </button>
+                {isLoggedIn === false ? [<></>
+                ] : [ <div className="flex flex-row items-center space-x-4">
+                    <button className="text-notgreen font-semibold flex flex-row border-white border-2 hover:border-notgreen h-fit p-2 rounded" 
+                            onClick={() => {navigate("/shop")}}
+                    ><Store className="mr-1"/>Shop</button>
+                    <button className="text-notgreen font-semibold flex flex-row border-white border-2 hover:border-notgreen h-fit p-2 rounded" 
+                            onClick={() => {navigate("/cart")}}
+                    ><ShoppingCart className="mr-1" />Cart</button>
+                    <button className="text-notgreen font-semibold flex flex-row border-white border-2 hover:border-notgreen h-fit p-2 rounded" 
+                            onClick={() => {navigate("/manage-orders")}}
+                    ><ShoppingBag className="mr-1"/>Manage Orders</button>
+                </div>]
+                }
+                
                 <div className="m-auto flex flex-auto">
                     {/* empty space */}
                 </div>
@@ -43,15 +57,6 @@ export default function Navbar() {
                     <button className="text-white bg-notblack rounded-sm h-fit py-2 px-10 mr-5" 
                     onClick={() => {navigate("/profile");}}
                     >My Profile</button>,
-                    <button className="text-white bg-notblack rounded-sm h-fit py-2 px-10 mr-5" 
-                    onClick={() => {navigate("/shop");}}
-                    >Shop</button>,
-                    <button className="text-white bg-notblack rounded-sm h-fit py-2 px-10 mr-5" 
-                    onClick={() => {navigate("/cart");}}
-                    >Cart</button>,
-                    <button className="text-white bg-notblack rounded-sm h-fit py-2 px-10 mr-5" 
-                    onClick={() => {navigate("/manage-orders");}}
-                    >Manage Orders</button>,
                     <button className="text-notblack bg-[#C8C8C8] rounded-sm h-fit px-10 py-2"
                     onClick={handleLogout}
                     >Log Out</button>
